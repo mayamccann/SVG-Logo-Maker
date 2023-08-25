d574928131e38e544e811f10b07cb31bbc7de4a2
Maya McCann <mccannmaya@gmail.com>
2023-08-25 16:42:42 -0400

    const to index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const questions = require('./lib/questions,js');
+const fileName = "logo.svg";
+const setShape = require('./lib/setShape.js')
