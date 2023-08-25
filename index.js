423c35d46f555977c40dfe3cf5a5c0ee29165738
Maya McCann <mccannmaya@gmail.com>
2023-08-25 16:45:37 -0400

    function createlogo response

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const fs = require('fs');
 const questions = require('./lib/questions,js');
 const fileName = "logo.svg";
 const setShape = require('./lib/setShape.js')
+
+function createLogo(response) {
+    const svg = setShape(response);
+    fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
+}
